79cf2ffe6ca62b9f9533d40908c8525d815b281a
SUDHANSHU4497 <sudhanshu.mriu@gmail.com>
2026-09-25 20:04:35 +0530

    introduction

---
+++ introduction.py
@@ -0,0 +1,43 @@
+# introduction variables
+name='sudhanshu' #"Sudhanshu"
+city='New Delhi'
+profession='Software Engineer'
+experience="3.5 years"
+education='B.Tech- computer Science'
+course='AI Engineering'
+interests=['coding', 'reading']
+goal= 'to become ai enggineer'
+hobbies=['traveling', 'music']
+language='English'
+
+
+# print variable
+print('------------simple print-----------------')
+print(name)
+print(city)
+print(profession)
+print(experience)
+print(education)
+print(course)
+print(goal)
+print(interests)
+print(hobbies)
+print(language)
+
+print('------------print with label-----------------')
+print("name:", name)
+print("city:", city)
+print("profession:", profession)
+print("experience:", experience)
+print("education:", education)
+print("course:", course)
+print("goal:", goal)
+print("interests:", interests)
+print("hobbies:", hobbies)
+print("language:", language)
+
+print('------------print with sep-----------------')
+print(name, city, profession, experience, education, course, goal, interests, hobbies, language, sep=", ")
+
+print('------------print with sep new line-----------------')
+print(name, city, profession, experience, education, course, goal, interests, hobbies, language, sep="\n ")
